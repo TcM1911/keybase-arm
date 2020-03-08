f7f62dbf891f75fcd225f05c76877b1505925457
TcM1911 <7453350+TcM1911@users.noreply.github.com>
2020-03-08 15:44:38 +0000

    Updates

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ fi
 
 
 function DownloadKeybase() {
-    go get -u -v $KBCLIENT_PATH/go/...
+    GOOS=linux GOARCH=arm go get -u -v $KBCLIENT_PATH/go/...
 }
 
 function CheckoutVersionAndPath() {
@@ -22,16 +22,27 @@ function CheckoutVersionAndPath() {
 }
 
 function BuildGO() {
-    GOOS=linux GOARCH=arm go build -o $BUILDROOT/target/keybase -v -tags production github.com/keybase/client/go/keybase
-    GOOS=linux GOARCH=arm go build -o $BUILDROOT/target/kbfsfuse -v -tags production github.com/keybase/client/go/kbfs/kbfsfuse
-    GOOS=linux GOARCH=arm go build -o $BUILDROOT/target/git-remote-keybase -v -tags production github.com/keybase/client/go/kbfs/kbfsgit/git-remote-keybase
+    GOOS=linux GOARCH=arm go build -o $BUILDROOT/target/keybase -tags production github.com/keybase/client/go/keybase
+    GOOS=linux GOARCH=arm go build -o $BUILDROOT/target/kbfsfuse -tags production github.com/keybase/client/go/kbfs/kbfsfuse
+    GOOS=linux GOARCH=arm go build -o $BUILDROOT/target/git-remote-keybase -tags production github.com/keybase/client/go/kbfs/kbfsgit/git-remote-keybase
 }
 
 function BuildGUI() {
     cd $GOPATH/src/$KBCLIENT_PATH/shared && \
        yarn install && \
        yarn run package -- --platform linux --arch armv7l --appVersion $VERSION && \
+       cp -r $GOPATH/src/$KBCLIENT_PATH/shared/desktop/release/linux-armv7l/Keybase-linux-armv7l $BUILDROOT/target/.
        cd $BUILDROOT
+}
+
+function CopyExtras() {
+    cp $GOPATH/src/$KBCLIENT_PATH/packaging/linux/{run_keybase,crypto_squirrel.txt,systemd/kbfs.service,systemd/keybase.gui.service,systemd/keybase.service} target/.
+}
+
+function Package() {
+    cd $BUILDROOT
+    mv target keybase-linux-arm-$VERSION
+    tar cfvz keybase-linux-arm-$VERSION.tar.gz keybase-linux-arm-$VERSION
 }
 
 
@@ -41,6 +52,8 @@ function main() {
     CheckoutVersionAndPath
     BuildGO
     BuildGUI
+    CopyExtras
+    Package
 }
 
 main
